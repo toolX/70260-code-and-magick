e1722f5da2c29fa3f49566696de671c070d9766b
Philip Shamsiev <ph.shamsiev@mail.ru>
2017-02-06 15:18:15 +0300

    Create module system for project

---
+++ js/colorize-element.js
@@ -0,0 +1,19 @@
+'use strict';
+
+window.colorizeElement = function (element, colors, property) {
+  var currentColor = colors[0];
+
+  var setRandomColor = function () {
+    while (window.utils.getRandomElementExcept(colors, currentColor)) {
+      currentColor = window.utils.getRandomElement(colors);
+    }
+    element.style[property] = currentColor;
+  };
+
+  element.addEventListener('click', setRandomColor);
+  element.addEventListener('keydown', function (event) {
+    if (window.utils.isEnterPressed(event)) {
+      setRandomColor();
+    }
+  });
+};
---
+++ js/utils.js
@@ -0,0 +1,23 @@
+'use strict';
+
+window.utils = {
+  ESCAPE_KEY_CODE: 27,
+  ENTER_KEY_CODE: 13,
+  isEnterPressed: function (event) {
+    return event.keyCode === window.utils.ENTER_KEY_CODE;
+  },
+  isEscapePressed: function (event) {
+    return event.keyCode === window.utils.ESCAPE_KEY_CODE;
+  },
+  escapeKeydowHandler: function (event) {
+    if (window.utils.isEscapePressed(event)) {
+      window.closeSetupDialog();
+    }
+  },
+  getRandomElement: function (array) {
+    return array[Math.floor(Math.random() * array.length)];
+  },
+  getRandomElementExcept: function (array, elem) {
+    return window.utils.getRandomElement(array) !== elem;
+  }
+};
